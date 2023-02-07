64b1a4057bc82341ea7030553c02f48caaebb78e
Guilherme Feuchard <98488671+Feuchard@users.noreply.github.com>
2023-02-06 21:03:14 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+const pecas = {
+  "bracos": {
+    "forca": 29,
+    "poder": 35,
+    "energia": -21,
+    "velocidade": -5,
+  },
+  "blindagem": {
+    "forca": 41,
+    "poder": 20,
+    "energia": 0,
+    "velocidade": -20,
+  },
+  "nucleos": {
+    "forca": 0,
+    "poder": 7,
+    "energia": 48,
+    "velocidade": -4,
+  },
+  "pernas": {
+    "forca": 27,
+    "poder": 21,
+    "energia": -32,
+    "velocidade": 43,
+  },
+  "foguetes": {
+    "forca": 0,
+    "poder": 28,
+    "energia": 0,
+    "velocidade": -2,
+  },
+}
+
+controle.forEach ( (elemento) => {
+  elemento.addEventListener("click", (evento) => {
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    atualizaEstatisticas(evento.target.dataset.peca)
+  })
+})
+
+function manipulaDados(operacao, controle) {
+  const peca = controle.querySelector("[data-contador]")
+
+  if(operacao === "-") {
+    peca.value = parseInt(peca.value) - 1
+  } else{
+    peca.value = parseInt(peca.value) + 1
+  }
+}
+
+function atualizaEstatisticas(peca){
+  console.log()
+
+  estatisticas.forEach( (elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) +  pecas[peca][elemento.dataset.estatistica]
+  })
+}
+
+/* -------- Trocar Cor robotron -------- */
+
+const robotron = document.querySelector("[data-robotron]"); const botaoCores = document.querySelectorAll("[data-cores]");
+
+botaoCores.forEach((cor) => { cor.addEventListener('click', (evento) =>{ 
+  robotron.src = evento.target.src }) 
+}) 
